9944862ffb0be6dda4c21761f472dd20f173b686
Amogh Mannekote <msamogh@gmail.com>
2024-01-10 19:11:35 -0500

    Add function to extract input variables

--- llm4qual/__init__.py
+++ llm4qual/__init__.py
@@ -1,13 +1,32 @@
 from typing import *
 from dataclasses import dataclass, field
 
+from transformers import Pipeline
 from datasets.features import ClassLabel, Features, Value
-import datasets.features as features
 from datasets.tasks.base import TaskTemplate
-
+from langchain.prompts import load_prompt
+from evaluate.evaluation_suite import SubTask
 import evaluate
 import datasets
-from evaluate.evaluation_suite import SubTask
+import datasets.features as features
+
+
+def extract_input_variables_from_prompt_template(
+    rubrics_to_prompt_templates: Mapping[Text, Sequence[Text]],
+    prompts_root_dir: Text,
+    suite_name: Text,
+) -> Sequence[Text]:
+    """Find the union of all input features over all prompt templates for each rubric."""
+    rubrics_to_input_variables = {}
+    for rubric, prompt_template_names in rubrics_to_prompt_templates.items():
+        input_variables = set()
+        for prompt_template_name in prompt_template_names:
+            for input_variable in load_prompt(
+                f"{prompts_root_dir}/{suite_name}/rubrics/{rubric.split('.')[0]}/{rubric.split('.')[1]}/{prompt_template_name}.yaml",
+            ).input_variables:
+                input_variables.add(input_variable)
+        rubrics_to_input_variables[rubric] = list(input_variables)
+    return rubrics_to_input_variables
 
 
 class LLMProxyConfig(datasets.BuilderConfig):
@@ -32,7 +51,7 @@ class LLMProxyEvaluationSuite(evaluate.EvaluationSuite):
         self.suite_name = suite_name
         self.rubrics_to_prompt_templates = rubrics_to_prompt_templates
         self.rubrics_to_input_variables = (
-            LLMProxyEvaluationSuite.extract_input_variables_from_prompt_template(
+            extract_input_variables_from_prompt_template(
                 rubrics_to_prompt_templates,
                 prompts_root_dir="prompts",
                 suite_name=suite_name,
@@ -46,7 +65,7 @@ class LLMProxyEvaluationSuite(evaluate.EvaluationSuite):
                 split=split_str,
                 args_for_task={
                     "metric": "mse"
-                    if self.task_type == "llm-proxy-regression"
+                    if task_type == "llm-proxy-regression"
                     else "accuracy",
                     "input_variables_column": input_variables,
                     "label_column": "label",
@@ -96,3 +115,24 @@ class LLMProxyClassificationTask(TaskTemplate):
             self.input_variables_column: "input_variables",
             self.label_column: "labels",
         }
+
+
+class LangchainPipeline(Pipeline):
+    def _sanitize_parameters(self, **kwargs):
+        print(f"SANITIZING {kwargs}")
+        if "chain" not in kwargs:
+            raise ValueError("chain must be specified")
+        self.chain = kwargs.pop("chain")
+
+    def preprocess(self, inputs: Dict[Text, Any]) -> Dict[Text, Any]:
+        print(f"PREPROCESSING {inputs}")
+        return inputs
+
+    def postprocess(self, model_outputs):
+        print(f"POSTPROCESSING {model_outputs}")
+        return model_outputs
+
+    def _forward(self, model_inputs):
+        print(f"FORWARDING {model_inputs}")
+        output = self.chain.invoke(model_inputs)
+        return output
